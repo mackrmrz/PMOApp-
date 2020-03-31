c91a56b504ed33c9f41dae0aba5f92924cb80915
Mack  Ramirez <mackramirez@Macks-MacBook-Pro.local>
2020-03-30 22:42:42 -0700

    Initial Commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <h1>Mack Attack</h1>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
+export default Header;
